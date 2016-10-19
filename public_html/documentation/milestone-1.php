dfa98b25019e5342f1a149f4f18125025aa656aa
Jonathan Guzman <jguzman41@cnm.edu>
2016-10-18 23:57:19 -0600

    added required .gitignore and README. Updated Persona

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+	<html lang="eng">
+	<head>
+		<meta charset="UTF-8">
+	<title>Stache House</title>
+	</head>
+	<body>
+<h1> Purpose, Audience, Goal</h1>
+		<p>
+		<ul>
+			<li>Purpose - to provide potential customers with a place where they can learn about the barbers, the location, schedule an appointmnet, and find directions.</li>
+			<li>Audience - Any potential customer seeking a barber in Albuquerque.</li>
+			<li>Goal - To increase sales and clientelle of the the Stache House</li>
+		</ul>
+		</p>
+<h1>Persona</h1>
+		This is Jesse. Jesse Needs a haircut. He is in his early 20's and just moved to Albuquerque. He has a big interview coming up nad needs to look sharp for it. He wants to get a low fade witha taper for a reasonable price, from a trusted barber. On his iPhone6 he googles "Albuquerque Barber Shops" and finds at least 50 different options. He opens a few tabs and checks out scheduling opoprtunities, reviews, and photo galleries. He would like to book an appointment, know who his barber is, and find directions to the barber shop
+	</body>
+
+</html>
