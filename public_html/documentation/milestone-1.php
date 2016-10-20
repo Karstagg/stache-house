81a2f8f47083e3d05c84c10604412b4acb8c3e3d
Jonathan Guzman <jguzman41@cnm.edu>
2016-10-19 23:46:24 -0600

    Finished Use Case and created index file.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,16 +6,17 @@
 	</head>
 	<body>
 <h1> Purpose, Audience, Goal</h1>
-		<p>
 		<ul>
 			<li>Purpose - to provide potential customers with a place where they can learn about the barbers, the location, schedule an appointmnet, and find directions.</li>
 			<li>Audience - Any potential customer seeking a barber in Albuquerque.</li>
 			<li>Goal - To increase sales and clientelle of the the Stache House</li>
 		</ul>
-		</p>
 <h1>Persona</h1>
-		<img src="" alt="jesse-persona">
-		This is Jesse (right). Jesse Needs a haircut. He is in his early 20's and just moved to Albuquerque. He has a big interview coming up nad needs to look sharp for it. He wants to get a low fade witha taper for a reasonable price, from a trusted barber. On his iPhone6 he googles "Albuquerque Barber Shops" and finds at least 50 different options. He opens a few tabs and checks out scheduling opoprtunities, reviews, and photo galleries. He would like to book an appointment, know who his barber is, and find directions to the barber shop.
+		<img src="images/jesse-persona.jpg" alt="jesse-persona">
+		<p>This is Jesse (right). Jesse Needs a haircut. He is a tech savvy 21 year old who just moved to Albuquerque. He has a big interview coming up and needs to look sharp for it. He wants to get a low fade witha taper for a reasonable price, from a trusted barber. On his iPhone6, with 4g LTE connection, he googles "Albuquerque Barber Shops" and finds at least 50 different options. He opens a few tabs and checks out scheduling opoprtunities, reviews, and photo galleries. He would like to book an appointment, know who his barber is, and find directions to the barber shop. It's tough being in a new city and finding a trustworthy barber. Getting it right the first time is incredibly important.</p>
+
+<h1>Use Case</h1>
+<p>In preparation for his interview, Jesse realizes his chances of getting hired are higher if he has a fressh, new haircut. Additonally, he'll feel more confident if it turns out well. Because he's new to the city, he has to bank on his new barber being able to do the job well. He doesn't know anyone in Albuquerque who he can get a reference from, so he turns to the web. After finding many possible options and reading all relevant reviews, he decides to open up a few of their web pages. Within 2 minutes, he lands on the page for the Stache House. On the page, he sees multiple customer testimonials showing positive reviews. He also sees a gallery of pictures showing perfectly executed haircuts. Finally, he sees the option to schedule an appointment, gets a price estimate, and sees a map to where he needs to go. After a day, he goes into see his barber, gets his haircut, and crushes his interview! Congratulations, Jesse. You just got hired. </p>
 	</body>
 
 </html>
