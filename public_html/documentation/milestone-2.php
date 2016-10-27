fa8950645d04a3cd9fef080ab2480a8b7f85c203
Jonathan Guzman <jguzman41@cnm.edu>
2016-10-27 09:36:31 -0600

    Added media directory, and completed milestone 2

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title> Milestone 2</title>
+		<body>
+		<h1>Content Strategy</h1>
+		<ul>
+			<li> Images - this is for showing off the atmosphere and feel of the shop. Important whne selecting where to get your hair cut</li>
+			<li> "Schedule an appointment" bar -  The barbers use a 3rd party scheduling software. Linking to that would be very helpful.</li>
+			<li> pictures of barbers - to establish some trust and introduce the barbers</li>
+			<li> "Follow us!" - the shop already ahs a strong SM presence, it's important to tie that in.</li>
+			<li> Location - having the google maps show where the shop is functions as a helpful and pretty tool, especially for new customers.</li>
+			<li> Contact us/Send us an email! - important for customer communication to the shop</li>
+		</ul>
+
+
+		<section>
+		<img src="../media/desktop-wireframe.png" alt="desktop-wireframe">
+		<img src="../media/stache-house-wireframe.png" alt="mobile-wireframe">
+		</section>
+	</body>
