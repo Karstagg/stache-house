0341bc21ec595d2837053a82dc5e277b00373f22
Jonathan Guzman <jguzman41@cnm.edu>
2016-12-01 09:10:55 -0700

    gitignore commit

--- public_html/index.php
+++ public_html/index.php
@@ -26,20 +26,23 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 
 		<!-- jQuery Form, Additional Methods, Validate -->
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
 
 		<!-- Your JavaScript Form Validator -->
 		<script src="js/form-validate.js"></script>
 
-<!--		custom jQuery validator-->
+		<!--		custom jQuery validator-->
 		<script src="js/jquery-validate.js" type="application/javascript"></script>
 
 		<!-- Google reCAPTCHA -->
 		<script src='https://www.google.com/recaptcha/api.js'></script>
 
-<!--		Google recaptcha-->
+		<!--		Google recaptcha-->
 		<script src='https://www.google.com/recaptcha/api.js'></script>
 
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
@@ -241,55 +244,59 @@
 			</div>
 		</div>
 
-		<!--		<instagram slidehsow>-->
-		<div class="container-fluid col-lg-8 col-lg-offset-2">
 
-			<!-- www.intagme.com -->
+		<!-- www.intagme.com -->
 
 
-			<!--			<input form>-->
-			<form id="contact-us" class="form-horizontal well" action="php/sendemail.php" method="post"
-					novalidate="novalidate">
-				<div class="form-group">
-					<label for="name">Name</label>
-					<div class="input-group">
-						<div class="input-group-addon">
-							<i class="glyphicon glyphicon-user" aria-hidden="true"></i>
-						</div>
-						<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+		<!--			<input form>-->
+
+		<!--Begin Contact Form-->
+		<form id="contact-form" action="php/mailer.php" method="post">
+			<div class="form-group">
+				<label for="name">Name <span class="text-danger">*</span></label>
+				<div class="input-group">
+					<div class="input-group-addon">
+						<i class="fa fa-user" aria-hidden="true"></i>
 					</div>
+					<input type="text" class="form-control" id="name" name="name" placeholder="Name">
 				</div>
-				<div class="form-group">
-					<label for="email">Email</label>
-					<div class="input-group">
-						<div class="input-group-addon">
-							<i class="glyphicon glyphicon-envelope" aria-hidden="true"></i>
-						</div>
-						<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+			</div>
+			<div class="form-group">
+				<label for="email">Email <span class="text-danger">*</span></label>
+				<div class="input-group">
+					<div class="input-group-addon">
+						<i class="fa fa-envelope" aria-hidden="true"></i>
 					</div>
+					<input type="email" class="form-control" id="email" name="email" placeholder="Email">
 				</div>
-				<div class="form-group">
-					<label for="subject">Subject</label>
-					<div class="input-group">
-						<div class="input-group-addon">
-							<i class="glyphicon glyphicon-modal-window" aria-hidden="true"></i>
-						</div>
-						<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+			</div>
+			<div class="form-group">
+				<label for="subject">Subject</label>
+				<div class="input-group">
+					<div class="input-group-addon">
+						<i class="fa fa-pencil" aria-hidden="true"></i>
 					</div>
+					<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
 				</div>
-				<div class="form-group">
-					<label for="message">Message</label>
-					<div class="input-group">
-						<div class="input-group-addon">
-							<i class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></i>
-						</div>
-						<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message"></textarea>
+			</div>
+			<div class="form-group">
+				<label for="message">Message <span class="text-danger">*</span></label>
+				<div class="input-group">
+					<div class="input-group-addon">
+						<i class="fa fa-comment" aria-hidden="true"></i>
 					</div>
+					<textarea class="form-control" rows="5" id="message" name="message"
+								 placeholder="Message (2000 characters max)"></textarea>
 				</div>
+			</div>
 
+			<!-- reCAPTCHA -->
+			<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
 
-				<!-- CAPTCHA -->
-				<div class="g-recaptcha" data-sitekey="6Lfhlg0UAAAAADFew4gxunMh_GySoa2oxUEzWju7"></div>
+			<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+			<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+		</form>
+
 		<!--empty area for form error/success output-->
 		<div class="row">
 			<div class="col-xs-12">
@@ -297,13 +304,63 @@
 			</div>
 		</div>
 
-		<div class="submit-and-reset-buttons text-center">
-			<button class="btn btn-warning" type="reset" title="Clear all fields in form"><i
-					class="glyphicon glyphicon-trash"></i> Reset
-			</button>
-			<button class="btn btn-success" type="submit" title="Send email"><i class="glyphicon glyphicon-send"></i> Send
-			</button>
-		</div>
+
+		<!--			<form id="contact-us" class="form-horizontal well" action="php/sendemail.php" method="post"-->
+		<!--					novalidate="novalidate">-->
+		<!--				<div class="form-group">-->
+		<!--					<label for="name">Name</label>-->
+		<!--					<div class="input-group">-->
+		<!--						<div class="input-group-addon">-->
+		<!--							<i class="glyphicon glyphicon-user" aria-hidden="true"></i>-->
+		<!--						</div>-->
+		<!--						<input type="text" class="form-control" id="name" name="name" placeholder="Name">-->
+		<!--					</div>-->
+		<!--				</div>-->
+		<!--				<div class="form-group">-->
+		<!--					<label for="email">Email</label>-->
+		<!--					<div class="input-group">-->
+		<!--						<div class="input-group-addon">-->
+		<!--							<i class="glyphicon glyphicon-envelope" aria-hidden="true"></i>-->
+		<!--						</div>-->
+		<!--						<input type="email" class="form-control" id="email" name="email" placeholder="Email">-->
+		<!--					</div>-->
+		<!--				</div>-->
+		<!--				<div class="form-group">-->
+		<!--					<label for="subject">Subject</label>-->
+		<!--					<div class="input-group">-->
+		<!--						<div class="input-group-addon">-->
+		<!--							<i class="glyphicon glyphicon-modal-window" aria-hidden="true"></i>-->
+		<!--						</div>-->
+		<!--						<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">-->
+		<!--					</div>-->
+		<!--				</div>-->
+		<!--				<div class="form-group">-->
+		<!--					<label for="message">Message</label>-->
+		<!--					<div class="input-group">-->
+		<!--						<div class="input-group-addon">-->
+		<!--							<i class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></i>-->
+		<!--						</div>-->
+		<!--						<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message"></textarea>-->
+		<!--					</div>-->
+		<!--				</div>-->
+		<!---->
+		<!---->
+		<!--				<!-- CAPTCHA -->-->
+		<!--				<div class="g-recaptcha" data-sitekey="6Lfhlg0UAAAAADFew4gxunMh_GySoa2oxUEzWju7"></div>-->
+		<!--		<!--empty area for form error/success output-->-->
+		<!--		<div class="row">-->
+		<!--			<div class="col-xs-12">-->
+		<!--				<div id="output-area"></div>-->
+		<!--			</div>-->
+		<!--		</div>-->
+		<!---->
+		<!--		<div class="submit-and-reset-buttons text-center">-->
+		<!--			<button class="btn btn-warning" type="reset" title="Clear all fields in form"><i-->
+		<!--					class="glyphicon glyphicon-trash"></i> Reset-->
+		<!--			</button>-->
+		<!--			<button class="btn btn-success" type="submit" title="Send email"><i class="glyphicon glyphicon-send"></i> Send-->
+		<!--			</button>-->
+		<!--		</div>-->
 
 		<!--		Create footer with follow me links-->
 		<nav class="navbar navbar-dark bg-inverse navbar-fixed-bottom">
